1e503efe6e03de2c23b5db409d0730a09fa0ae8b
FlavioSolon <solondutra73@gmail.com>
2026-04-24 17:52:35 -0400

    melhorias no backend

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -15,6 +15,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
         ]);
+        $middleware->api(prepend: [
+            \Illuminate\Http\Middleware\HandleCors::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->renderable(function (\Illuminate\Auth\AuthenticationException $e, $request) {
